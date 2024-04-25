dd87277aeb71747fe5461616628a8995cbed1721
Justin Tan <justintws10@gmail.com>
2024-04-25 20:44:20 +1000

    added http.rb

---
+++ 2-http.rb
@@ -0,0 +1,19 @@
+require 'socket'
+
+server = TCPServer.new(1337)
+
+loop do
+  client = server.accept
+
+  request_line = client.readline
+
+  puts "The HTTP request line looks like this:"
+  puts request_line
+
+  method_token, target, version_number = request_line.split
+  response_body = "✅ Received a #{method_token} request to #{target} with version #{version_number}"
+
+  client.puts response_body
+
+  client.close
+end
